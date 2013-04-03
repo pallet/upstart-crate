cf48f5338eeb4835de4af96c3fb5ff96dc7f7571
Hugo Duncan <hugo@hugoduncan.org>
2013-04-03 12:16:51 -0400

    Make codox link to correct source

--- profiles.clj
+++ profiles.clj
@@ -11,20 +11,17 @@
             "live-test" ["do" "live-test-up," "live-test-down"]}
   :test-selectors {:default (complement :live-test)
                    :live-test :live-test
-                   :all (constantly true)}
-  :repositories {"boundary-site" "http://maven.boundary.com/artifactory/repo"}}
+                   :all (constantly true)}}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0-SNAPSHOT"]]
        :plugins [[codox/codox.leiningen "0.6.4"]
                  [lein-marginalia "0.7.1"]]
        :codox {:writer codox-md.writer/write-docs
                :output-dir "doc/0.8/api"
-               :src-dir-uri "https://github.com/pallet/java-crate/blob/develop"
+               :src-dir-uri "https://github.com/pallet/upstart-crate/blob/develop"
                :src-linenum-anchor-prefix "L"}
        :aliases {"marg" ["marg" "-d" "doc/0.8/annotated"]
                  "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}
-       }
- :pallet {:jvm-opts ["-Djna.nosys=true"]}
+                 "doc" ["do" "codox," "marg"]}}
  :release
  {:set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}}
